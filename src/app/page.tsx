7793f5d3a691b507f334cb0e99db3f4dcb3d8819
johnmarestimada <johnmar11.jme@gmail.com>
2025-02-28 08:05:13 +0800

    added login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,52 @@
-import Image from "next/image";
-
+import Link from "next/link";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex items-center justify-center h-screen bg-[#EAF4FA] relative"> 
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+<img 
+        src="/logo.svg" 
+        alt="Logo" 
+        className="w-60 h-60 absolute top-32 right-80 z-20"></img>
+
+      <div className="bg-white p-10 rounded-xl shadow-lg w-[700px] h-[500px] relative z-10">
+        <div className="text-cyan-500 text-sm cursor-pointer mb-6">&lt; Back</div>
+        
+        <h1 className="text-3xl font-extrabold mb-6">Login</h1>
+        
+        <label className="block font-bold mb-2">Email</label>
+        <div className="flex items-center border-2 rounded-lg px-3 py-2 mb-4 w-72">
+          <span className="text-gray-400">@</span>
+          <input 
+            type="email" 
+            className="outline-none w-60 ml-2" 
+            placeholder="Enter your email"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <label className="block font-bold mb-2">Password</label>
+        <div className="flex items-center border-2 rounded-lg px-3 py-2 mb-4 w-72">
+        <span className="text-gray-400">@</span>
+        <input 
+          type="password" 
+          className="outline-none w-60 ml-2" 
+          placeholder="Enter your password"
+        />
+        </div>
+        
+        <div className="text-cyan-500 text-sm text-left mb-6 cursor-pointer">Forgot your password?</div>
+        
+        <button className="float-right w-60 bg-cyan-400 text-white font-bold py-2 rounded-lg shadow-md hover:bg-cyan-500 transition-all">
+          Login
+        </button>
+        
+        <div className="text-center text-sm text-gray-500 mt-4 absolute bottom-5 w-full">
+          Don't have an account? 
+          <Link href="/registration">
+            <span className="text-cyan-500 cursor-pointer"> Sign up here.</span>
+          </Link>
+        </div>
+      </div>
+
+      <div className="hidden md:block bg-cyan-300 h-[500px] w-[250px] absolute top-1/2 transform -translate-y-1/2 right-44 rounded-lg"></div>
     </div>
   );
 }
